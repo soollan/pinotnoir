ec9d9e8c22d3098cf7472a48c5d7f918f0ab8fac
soollan <soollan@greencar.co.kr>
2021-11-12 13:26:39 +0900

    init

---
+++ src/main/java/com/wine/pinotnoir/PinotnoirApplication.java
@@ -0,0 +1,13 @@
+package com.wine.pinotnoir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinotnoirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinotnoirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wine/pinotnoir/PinotnoirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wine.pinotnoir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinotnoirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
